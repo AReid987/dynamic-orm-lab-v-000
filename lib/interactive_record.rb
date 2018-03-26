c43f852e1be1a75c31261459ebd8e3e1a3766193
Antonio Reid <read.musik@gmail.com>
2018-03-26 21:11:05 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -14,6 +14,11 @@ class InteractiveRecord
     table_info = DB[:conn].execute(sql)
 
     column_names = []
+
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    
     binding.pry
   end
 end
